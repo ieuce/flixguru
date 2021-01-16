e6a1e4895840e52b1e0f43c7c826727814d82b64
Goodsea <kutsal_baran@hotmail.com>
2021-01-16 03:02:03 +0300

    Create multiple methods from codes in main method

--- src/Data.java
+++ src/Data.java
@@ -3,37 +3,10 @@ import org.apache.commons.lang3.ArrayUtils;
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
-        String line;
-        final int n_components = 2;
-
-        String data_dir = "F:\\PROJECTS\\PROCESSING\\suggest2me\\data\\ml-25m\\";
-        String movies_path = data_dir+"movies.csv";
-        String genome_tags_path = data_dir+"genome-tags.csv";
-        String genome_scores_path = data_dir+"genome-scores.csv";
-
-        Map<Integer, String> movie_map = new LinkedHashMap<Integer, String>();
+public class Data {
+    public static Map<Integer, String> genomeID_Tag(String genome_tags_path){
         Map<Integer, String> all_genome_map = new LinkedHashMap<Integer, String>();
-        Map<Integer, double[]> movieGenomeScore_map = new LinkedHashMap<Integer, double[]>();
-
-        // Movies Data Frame
-        try {
-            BufferedReader movies_df = new BufferedReader(new FileReader(movies_path));
-            boolean first_line = true;
-            while ((line = movies_df.readLine()) != null) {
-                if(first_line){
-                    first_line = false;
-                } else {
-                    String[] metadata = line.split(",");
-                    movie_map.put(Integer.parseInt(metadata[0]), metadata[1]);
-                }
-            }
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-
-        // Genome Tags Data Frame
+        String line;
         try {
             BufferedReader genome_tags_df = new BufferedReader(new FileReader(genome_tags_path));
             boolean first_line = true;
@@ -49,7 +22,13 @@ public class Main {
             e.printStackTrace();
         }
 
-        // Genome Scores Data Frame
+        return all_genome_map;
+    }
+
+    public static Map<Integer, double[]> movieID_Encodings(String genome_scores_path, String genome_tags_path){
+        Map<Integer, String> all_genome_map = genomeID_Tag(genome_tags_path);
+        Map<Integer, double[]> movieGenomeScore_map = new LinkedHashMap<Integer, double[]>();
+        String line;
         try {
             BufferedReader genome_scores_df = new BufferedReader(new FileReader(genome_scores_path));
             line = genome_scores_df.readLine();
@@ -69,8 +48,11 @@ public class Main {
         }catch (IOException e){
             e.printStackTrace();
         }
+        return movieGenomeScore_map;
+    }
 
-        // Test PrincipalComponentAnalysis
+    public static double[][] get_encodings(String genome_scores_path, String genome_tags_path){
+        Map<Integer, double[]> movieGenomeScore_map = movieID_Encodings(genome_scores_path, genome_tags_path);
         double[][] encodings = new double[movieGenomeScore_map.keySet().size()][];
 
         int ith = 0;
@@ -78,10 +60,22 @@ public class Main {
             encodings[ith] = movieGenomeScore_map.get(key);
             ith++;
         }
+        return encodings;
+    }
 
+    public static void main(String[] args) {
+        final int n_components = 2;
+
+        String data_dir = "F:\\PROJECTS\\PROCESSING\\suggest2me\\data\\ml-25m\\";
+        String genome_tags_path = data_dir+"genome-tags.csv";
+        String genome_scores_path = data_dir+"genome-scores.csv";
+
+        Map<Integer, double[]> movieGenomeScore_map = movieID_Encodings(genome_scores_path, genome_tags_path);
+
+        double[][] encodings = get_encodings(genome_scores_path, genome_tags_path);
         PrincipalComponentAnalysis pca = new PrincipalComponentAnalysis(n_components);
-
         pca.fit(encodings);
+
         double[][] transformed_encodings1 = pca.transform(pca.expand_dims(movieGenomeScore_map.get(4), true)); //4,Waiting to Exhale (1995),Comedy|Drama|Romance
         System.out.println(ArrayUtils.toString(transformed_encodings1[0]));
         double[][] transformed_encodings2 = pca.transform(pca.expand_dims(movieGenomeScore_map.get(11), true)); // 11,"American President, The (1995)",Comedy|Drama|Romance
